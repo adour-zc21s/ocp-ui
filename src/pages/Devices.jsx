8f8c6824f82126992830eddb0db5fa2da85f2ea9
adour-zc21s <wahid.asimetris@gmail.com>
2026-07-08 13:53:27 +0700

    change font

--- src/pages/Devices.jsx
+++ src/pages/Devices.jsx
@@ -463,7 +463,8 @@ const Devices = () => {
                     <button
                         title="Add Device"
                         type="button"
-                        className="w-full sm:w-auto text-green-500 px-3 py-2 rounded-xl text-xs bg-green-200 hover:bg-green-300 transition duration-200"
+                        // className="w-full sm:w-auto text-green-500 px-3 py-2 rounded-xl text-xs bg-green-200 hover:bg-green-300 transition duration-200"
+                        className="text-green-500 px-3 py-2 rounded-xl text-xs bg-green-200 hover:bg-green-300 transition duration-200"
                         onClick={handleOpenAddModal}
                     >
                         New Device
