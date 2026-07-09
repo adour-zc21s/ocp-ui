25f74cc0954cec19790372f04cd121dae69b798f
adour-zc21s <wahid.asimetris@gmail.com>
2026-07-09 11:42:04 +0700

    'no-undef': 0,

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,7 @@ module.exports = {
     'linebreak-style': 0,
     'react/state-in-constructor': 0,
     'import/prefer-default-export': 0,
+    'no-undef': 0,
     'padded-blocks': 0,
     'eol-last': 0,
     'max-len': [
